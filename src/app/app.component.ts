4335d173b4904c603b91575e36c41b60c8d914f1
Kapilan Srikaran <kapilansrikaran@gmail.com>
2024-07-03 15:58:41 +0530

    angular v18 routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {  RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CrisisListComponent } from "./crisis-list/crisis-list.component";
+import { HeroesListComponent } from "./heroes-list/heroes-list.component";
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, CrisisListComponent, HeroesListComponent,AppComponent,RouterLink,RouterLinkActive]
 })
 export class AppComponent {
   title = 'routing-app';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', redirectTo: '/heroes-list', pathMatch: 'full'},
+    {path: 'crisis-list', component: CrisisListComponent},
+    {path: 'heroes-list', component: HeroesListComponent},
+
+    // The new route uses a path, **. This path is how Angular identifies a wildcard route. 
+    // - Any route that does not match an existing route in your configuration will use this route.
+    // IMPORTANT: Notice that the wildcard route is placed at the end of the array. 
+    // - The order of your routes is important, as Angular applies routes in order and uses the first match it finds.
+    {path: '**', component: PageNotFoundComponent}
+    
+];
